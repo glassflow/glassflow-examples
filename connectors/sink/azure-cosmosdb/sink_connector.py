a1e52f2aadb55e4d693fb19251cb747a92fe2ddd
boburumurzokov <boburmirzo.umurzokov@gmail.com>
2024-05-10 10:46:30 +0300

    Add readme file

--- connectors/sink/azure-cosmosdb/sink_connector.py
+++ connectors/sink/azure-cosmosdb/sink_connector.py
@@ -1,13 +1,12 @@
 import os
 import azure.cosmos.cosmos_client as cosmos_client
 import azure.cosmos.errors as errors
-import azure.cosmos.http_constants as http_constants
-import azure.cosmos.documents as documents
 from azure.cosmos.partition_key import PartitionKey
 import time
 import random
 import glassflow
 from dotenv import load_dotenv
+import uuid
 
 
 class SinkConnectorCosmosDB:
@@ -29,34 +28,41 @@ class SinkConnectorCosmosDB:
     def _create_database_if_not_exists(self):
         # Creating database
         try:
-            self.cosmos_db = self.cosmos_client.create_database(id=self.cosmosdb_database_name)
-            print('Database with id \'{0}\' created'.format(self.cosmosdb_database_name))
+            self.cosmos_db = self.cosmos_client.create_database(
+                id=self.cosmosdb_database_name
+            )
+            print("Database with id '{0}' created".format(self.cosmosdb_database_name))
         except errors.CosmosResourceExistsError:
-            print('A database with id \'{0}\' already exists'.format(self.cosmosdb_database_name))
-            self.cosmos_db = self.cosmos_client.get_database_client(database=self.cosmosdb_database_name)
+            print(
+                "A database with id '{0}' already exists".format(
+                    self.cosmosdb_database_name
+                )
+            )
+            self.cosmos_db = self.cosmos_client.get_database_client(
+                database=self.cosmosdb_database_name
+            )
         except Exception as e:
-            print('Error creating database: ', e)
-    
-
+            print("Error creating database: ", e)
 
     def _create_container_if_not_exists(self):
-        partition_key = PartitionKey(path='/id', kind='Hash')
+        partition_key = PartitionKey(path="/user_id")
         container_name = self.cosmosdb_container_name
         try:
             self.cosmos_db_container = self.cosmos_db.create_container(
-                id=container_name,
-                partition_key=partition_key
+                id=container_name, partition_key=partition_key
             )
-            print('Container with id \'{0}\' created'.format(container_name))
+            print("Container with id '{0}' created".format(container_name))
         except errors.CosmosResourceExistsError:
-            print('A container with id \'{0}\' already exists'.format(container_name))
-            self.cosmos_db_container = self.cosmos_db.get_container_client(container_name)
+            print("A container with id '{0}' already exists".format(container_name))
+            self.cosmos_db_container = self.cosmos_db.get_container_client(
+                container_name
+            )
         except Exception as e:
-            print('Error creating container: ', e)
+            print("Error creating container: ", e)
 
     def put(self, data):
-        container_link = f"dbs/{self.cosmosdb_database_name}/colls/{self.cosmosdb_container_name}"
-        self.cosmos_db_container.upsert_item(data)
+        data["id"] = str(uuid.uuid4())
+        self.cosmos_db_container.upsert_item(body=data)
 
     def cleanup(self):
         pass
