203ac18033a8960623b2d43b0a6022a691832f47
boburumurzokov <boburmirzo.umurzokov@gmail.com>
2024-06-07 15:56:15 +0300

    update producer and consumer

--- docs/develop/consumer-data/consumer.py
+++ docs/develop/consumer-data/consumer.py
@@ -1,25 +1,34 @@
 import glassflow
+import sys
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
-pipeline_id = os.getenv("PIPELINE_ID")
-space_id = os.getenv("SPACE_ID")
-token = os.getenv("PIPELINE_ACCESS_TOKEN")
+def main():
+    pipeline_id = os.environ["PIPELINE_ID"]
+    space_id = os.environ["SPACE_ID"]
+    token = os.environ["PIPELINE_ACCESS_TOKEN"]
 
-# 1. Initializes a GlassFlow Python client
-client = glassflow.GlassFlowClient()
-# 2. Creates a pipeline
-pipeline_client = client.pipeline_client(
-    space_id=space_id, pipeline_id=pipeline_id, pipeline_access_token=token
-)
+    # 1. Initializes a GlassFlow Python client
+    client = glassflow.GlassFlowClient()
+    # 2. Creates a pipeline
+    pipeline_client = client.pipeline_client(
+        space_id=space_id, pipeline_id=pipeline_id, pipeline_access_token=token
+    )
 
-# 3. Consumes a new event from the pipeline
-response = pipeline_client.consume()
-print(response)
-if response.status_code == 200:
-    data = response.json()
-    print("Consumed Data:", data)
-else:
-    print(f"Failed to consume data from GlassFlow: {response.text}")
+    while True:
+        try:
+            # 3. Consumes a new event from the pipeline
+            response = pipeline_client.consume()
+
+            if response.status_code == 200:
+                data = response.body.event
+                print("Consumed Data:", data)
+        except KeyboardInterrupt:
+            print("exiting")
+            sys.exit(0)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    main()
