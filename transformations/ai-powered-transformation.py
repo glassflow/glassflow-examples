e90e98b5a9e9112e50464a0443eae64f9ea7520e
Bobur Umurzokov <boburmirzo.umurzokov@gmail.com>
2024-07-21 17:20:58 +0300

    Update ai-powered-transformation.py

--- transformations/ai-powered-transformation.py
+++ transformations/ai-powered-transformation.py
@@ -1,15 +1,18 @@
 import openai
 import json
 
-
 def handler(data, log):
+    """
+    Generate AI insights to predict car price data.
+    """
     log.info("Event:" + json.dumps(data), data=data)
 
-    insights = detect_anomalies(data)
+    insights = predict_price(data)
+    
     return json.loads(insights)
 
 
-def detect_anomalies(log):
+def predict_price(data):
     openai.api_key = "{REPLACE_WITH_YOUR_OPENAI_API_KEY}"
     response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -17,14 +20,14 @@ def detect_anomalies(log):
         messages=[
             {
                 "role": "system",
-                "content": "You are a great data analyst to detect anomalies on server logs.",
+                "content": "You are a great data analyst to predict car data prices.",
             },
             {
                 "role": "user",
-                "content": f"Analyze the following log: {log}, identify if it is normal, unusual or suspicious",
+                "content": f"Analyze the following vehicle data: {data}, predict its price and return only the original full JSON with additional suggested_price attribute and value added",
             },
         ],
-        max_tokens=100,
+        max_tokens=500,
         temperature=0.5,
     )
 
