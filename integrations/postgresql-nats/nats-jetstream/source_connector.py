09e76506188ebf3b72d867c42f0ebc6c3ba8952a
boburumurzokov <boburmirzo.umurzokov@gmail.com>
2024-07-22 21:32:55 +0300

    Update with new code sample

--- integrations/postgresql-nats/nats-jetstream/source_connector.py
+++ integrations/postgresql-nats/nats-jetstream/source_connector.py
@@ -7,21 +7,36 @@ import glassflow
 
 
 class NatsJetstreamSourceConnector:
-    def __init__(self, nats_jetsream_subjects, glassflow_client):
+    def __init__(
+        self,
+        nats_jetsream_subjects,
+        glassflow_client,
+    ):
         self.glassflow_client = glassflow_client
         self.nats_subject = nats_jetsream_subjects
         self.loop = asyncio.get_event_loop()
 
     async def on_event(self, msg):
-        event_data_json = json.loads(msg.data.decode("utf-8"))
-        response = self.glassflow_client.publish(request_body=event_data_json)
+        event_data_json = json.loads(
+            msg.data.decode("utf-8")
+        )
+        response = self.glassflow_client.publish(
+            request_body=event_data_json
+        )
         if response.status_code == 200:
-            print("Data sent successfully to GlassFlow")
+            print(
+                "Data sent successfully to GlassFlow"
+            )
+            await msg.ack()
         else:
-            print(f"Failed to send data to GlassFlow: {response.text}")
+            print(
+                f"Failed to send data to GlassFlow: {response.text}"
+            )
 
     async def receive(self, js):
-        await js.subscribe(self.nats_subject, cb=self.on_event)
+        await js.subscribe(
+            self.nats_subject, cb=self.on_event
+        )
 
 
 async def main():
@@ -29,21 +44,27 @@ async def main():
     load_dotenv()
 
     pipeline_id = os.getenv("PIPELINE_ID")
-    space_id = os.getenv("SPACE_ID")
-    pipeline_access_token = os.getenv("PIPELINE_ACCESS_TOKEN")
-    nats_jetsream_url = os.getenv("NATS_JETSREAM_URL")
-    nats_jetsream_subjects = os.getenv("NATS_JETSREAM_SUBJECTS")
+    pipeline_access_token = os.getenv(
+        "PIPELINE_ACCESS_TOKEN"
+    )
+    nats_jetsream_url = os.getenv(
+        "NATS_JETSREAM_URL"
+    )
+    nats_jetsream_subjects = os.getenv(
+        "NATS_JETSREAM_SUBJECTS"
+    )
 
     # Initialize the GlassFlow client
     glassflow_client = glassflow.GlassFlowClient().pipeline_client(
-        space_id=space_id,
         pipeline_id=pipeline_id,
         pipeline_access_token=pipeline_access_token,
     )
 
     # Initialize the NATS Jetstream
     nc = await nats.connect(nats_jetsream_url)
-    print(f"Connected to NATS at {nc.connected_url.netloc}...")
+    print(
+        f"Connected to NATS at {nc.connected_url.netloc}..."
+    )
     js = nc.jetstream()
 
     # Initialize the NatsJetsream source connector with the GlassFlow client
