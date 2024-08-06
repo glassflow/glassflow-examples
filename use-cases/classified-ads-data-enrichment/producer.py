225243f22d911649345c957fe4e113578ea207a5
Pablo Pardo Garcia <pablo@glassflow.dev>
2024-08-06 14:54:48 +0200

    load data from classified ads dataset

--- use-cases/classified-ads-data-enrichment/producer.py
+++ use-cases/classified-ads-data-enrichment/producer.py
@@ -1,25 +1,28 @@
-import base64
+from dotenv import load_dotenv
 import glassflow
 import json
 import os
-from dotenv import load_dotenv
-
-
-def encode_image(image_path: str):
-    with open(image_path, "rb") as f:
-        return base64.b64encode(f.read()).decode('utf-8')
+import sys
 
 
 def send_message_to_glassflow(request_body, glassflow_client):
     response = glassflow_client.publish(request_body=request_body)
     if response.status_code == 200:
-        print("Message sent to GlassFlow:", request_body)
+        print("Sent message to GlassFlow with ad ID:", request_body["id"])
     else:
         print(f"Failed to send data to GlassFlow: {response.text}")
 
 
-def main():
-    # Initialize GlassFlow client
+def get_ads_data(path):
+    for file in os.listdir(path):
+        if file.endswith(".json"):
+            print("Loading data from file:", file)
+            data = json.load(open(path + file, "r"))
+            for x in data:
+                yield x
+
+
+def main(data_path: str):
     pipeline_id = os.environ["PIPELINE_ID"]
     pipeline_access_token = os.environ["PIPELINE_ACCESS_TOKEN"]
     glassflow_client = glassflow.GlassFlowClient().pipeline_client(
@@ -27,19 +30,10 @@ def main():
         pipeline_access_token=pipeline_access_token,
     )
 
-    ads = json.load(open("data/classified-ads-examples.json", "r"))
-    for ad in ads:
-        # Read images and send them to the pipeline base64 encoded
-        if "images" in ad and len(ad["images"]) > 0:
-            images = []
-            for img in ad["images"]:
-                img_b64v = encode_image(img["path"])
-                images.append(dict(format=img["format"], b64=img_b64v))
-            ad["images"] = images
-
+    for ad in get_ads_data(data_path):
         send_message_to_glassflow(ad, glassflow_client)
 
 
 if __name__ == "__main__":
     load_dotenv()
-    main()
+    main(data_path=sys.argv[1])
