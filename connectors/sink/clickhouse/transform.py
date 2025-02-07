a3b06547dd028dea7468284fbc2daf06e6d03a5f
Ashish Bagri <ashishbagri@gmail.com>
2025-02-07 16:22:14 +0100

    add more context in clickhouse transform.py

--- connectors/sink/clickhouse/transform.py
+++ connectors/sink/clickhouse/transform.py
@@ -3,5 +3,8 @@ import json
 
 def handler(data, log):
     log.info("Echo: " + json.dumps(data))
-
-    return data
+    # expected data for clickhouse sink is a dictionary where each key is a column name and value is the column value
+    # Clickhouse accepts only insert
+    # event = {"column_name_1": "column_value_1", "column_name_2": "column_value_2"}
+    clickhouse_event = {}
+    return clickhouse_event
